89d0c3fc0535aa506e54b85bc6f256beea0b8bff
Dmytro Petrykiv <dmytro.petrykiv@ale.ai>
2025-05-06 15:04:03 +0300

    Implemented settings for DB (postgres)

--- src/database/core/config.py
+++ src/database/core/config.py
@@ -1,2 +1,25 @@
-def get_db_url():
-    pass
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseSettings):
+    USER: str
+    PASSWORD: str
+    DB: str
+    HOST: str
+    PORT: str
+
+    model_config = SettingsConfigDict(
+        env_prefix='POSTGRES_',
+        env_file="./.env"
+    )
+
+
+@lru_cache
+def get_database_settings() -> DatabaseSettings:
+    return DatabaseSettings()
+
+def get_db_url() -> str:
+    conf = get_database_settings()
+    return f"postgresql+asyncpg://{conf.USER}:{conf.PASSWORD}@{conf.HOST}:{conf.PORT}/{conf.DB}"
