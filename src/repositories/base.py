45d55dbf527455e55fb9827861abdebff9191d78
Dmytro Petrykiv <dmytro.petrykiv@ale.ai>
2025-05-06 15:23:09 +0300

    Fixed base repo (add method)

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+from loguru import logger
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -20,9 +21,10 @@ class BaseRepository(ABC):
         pass
 
     async def add(self, obj_data: dict):
-        new_obj = await self._session.execute(
-            insert(self.model).values(**obj_data)
+        new_obj = await self._session.scalar(
+            insert(self.model).values(**obj_data).returning(self.model)
         )
+        logger.info(f"Created new obj {new_obj}")
         return new_obj
 
     async def delete(self):
