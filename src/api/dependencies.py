ffd0aa25b8ca709ceabee2294f3101818fbafa67
Dmytro Petrykiv <dmytro.petrykiv@ale.ai>
2025-05-06 15:03:38 +0300

    Added dependencies (service + repo + db) for user

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,5 +4,22 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.core.engine import get_async_session
+from src.repositories.user import UserRepository
+from src.services.user import UserService
 
 AsyncSessionDep = Annotated[AsyncSession, Depends(get_async_session)]
+
+
+def get_user_repository(
+    session: AsyncSessionDep
+) -> UserRepository:
+    return UserRepository(session=session)
+
+UserRepoDep = Annotated[UserRepository, Depends(get_user_repository)]
+
+def get_user_service(
+    user_repo: UserRepoDep
+):
+    return UserService(user_repo=user_repo)
+
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
