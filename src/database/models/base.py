af1e4f81c49c9ba82d97886d00402e615275741c
Dmytro Petrykiv <dmytro.petrykiv@ale.ai>
2025-05-06 15:22:20 +0300

    Fixed base sql model

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,8 +1,8 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
 
 class IdModelMixin:
-    id: Mapped[UUID] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
