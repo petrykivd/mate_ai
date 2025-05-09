e0099030e881ccc9390218d167e572686a80e717
Dmytro Petrykiv <dmytro.petrykiv@ale.ai>
2025-05-09 17:12:52 +0300

    Update orm mixin (created_at)

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,8 +1,11 @@
+from datetime import datetime as dt
 from uuid import UUID, uuid4
 
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
 
-class IdModelMixin:
+class IdCreatedAtModelMixin:
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    created_at: Mapped[dt] = mapped_column(DateTime(timezone=True), default=func.now())
